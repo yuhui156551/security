761dba09e35a039e1eca93fa5cc08e0715d1a350
yuhui <536430462@qq.com>
2023-02-04 22:38:48 +0800

    配置授权

---
+++ security10authorize/src/main/java/com/yuhui/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.yuhui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    // 创建内存数据源
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("root").password("{noop}123").roles("ADMIN").build());
+        inMemoryUserDetailsManager.createUser(User.withUsername("yuhui").password("{noop}123").roles("USER").build());
+        inMemoryUserDetailsManager.createUser(User.withUsername("lisi").password("{noop}123").roles("READ_INFO").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/getInfo").hasRole("READ_INFO")
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().csrf().disable();
+    }
+}

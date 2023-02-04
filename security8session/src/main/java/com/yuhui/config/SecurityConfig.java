cf7d2a20779cc9018ba530510a2a0edf83a7a12d
yuhui <536430462@qq.com>
2023-02-04 13:29:40 +0800

    会话失效处理

--- security8session/src/main/java/com/yuhui/config/SecurityConfig.java
+++ security8session/src/main/java/com/yuhui/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.yuhui.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yuhui
@@ -22,7 +27,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .sessionManagement()// 开启会话管理
-                .maximumSessions(1);// 会话并发数
+                .maximumSessions(1)
+                // 会话过期处理
+                .expiredSessionStrategy(event -> {
+                    HttpServletResponse response = event.getResponse();
+                    Map<String, Object> result = new HashMap<>();
+                    result.put("status", 500);
+                    result.put("msg", "当前会话已经失效,请重新登录!");
+                    String s = new ObjectMapper().writeValueAsString(result);
+                    response.setContentType("application/json;charset=UTF-8");
+                    response.getWriter().println(s);
+                    response.flushBuffer();
+                });
     }
 
     @Bean

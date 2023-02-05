bb142333f50e57b40408d6cbf7f1d28b8da7c4dd
yuhui <536430462@qq.com>
2023-02-05 19:12:06 +0800

    OAuth2 github实例

---
+++ security12oauth-client-github/src/main/java/com/yuhui/Security12oauthClientGithubApplication.java
@@ -0,0 +1,13 @@
+package com.yuhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security12oauthClientGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security12oauthClientGithubApplication.class, args);
+	}
+
+}

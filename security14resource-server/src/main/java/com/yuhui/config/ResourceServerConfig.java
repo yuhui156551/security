7f12a99da17d6d3f53880f528a8f3a64a5c132be
yuhui <536430462@qq.com>
2023-02-05 22:46:00 +0800

    资源服务器 搭建

---
+++ security14resource-server/src/main/java/com/yuhui/config/ResourceServerConfig.java
@@ -0,0 +1,33 @@
+package com.yuhui.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    private final DataSource dataSource;
+
+    @Autowired
+    public ResourceServerConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+}
---
+++ security14resource-server/src/test/java/com/yuhui/Security14resourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security14resourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
